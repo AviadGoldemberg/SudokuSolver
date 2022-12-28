51ea36ba3b10fa7a1946a872ba4d594dd9ba0721
Aviad <aviadgolde@gmail.com>
2022-12-28 21:01:08 +0200

    create abstract class for sudoku solvers and solving result struct

---
+++ SudokuSolver/Solvers/ISudokuSolver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SudokuSolver.Board;
+
+namespace SudokuSolver.Solvers
+{
+    /// <summary>
+    /// Abstract class which represent a sudoku solver.
+    /// </summary>
+    internal abstract class ISudokuSolver
+    {
+        protected ISudokuBoard board;
+        public ISudokuSolver(ISudokuBoard board)
+        {
+            this.board = board;
+        }
+        public abstract SolvingResult Solve();
+
+    }
+}
