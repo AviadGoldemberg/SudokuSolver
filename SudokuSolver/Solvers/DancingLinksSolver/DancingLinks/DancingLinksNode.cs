b91648a34de52ec0a7da54908a6c96ac75243616
Aviad <aviadgolde@gmail.com>
2022-12-28 21:25:23 +0200

    create dancing links node class

---
+++ SudokuSolver/Solvers/DancingLinksSolver/DancingLinks/DancingLinksNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Solvers.DancingLinksSolver
+{
+    internal class DancingLinksNode
+    {
+        public DancingLinksNode Left { get; set; }
+        public DancingLinksNode Right { get; set; }
+        public DancingLinksNode Up { get; set; }
+        public DancingLinksNode Down { get; set; }
+        public int Size { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+    }
+}
