8cf25745007ada53f829af8f64cfcb15f14358e1
Aviad <aviadgolde@gmail.com>
2023-01-04 21:05:11 +0200

    Create testing class for Sudoku solver.

---
+++ SudokuTest/SudokuSolveTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SudokuTest
+{
+    [TestClass]
+    public class SudokuSolveTest
+    {
+        [TestMethod]
+        public void Solve_ValidBoards_true()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
